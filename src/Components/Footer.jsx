aef168296cf36b1a042e07707eeca44e72f934cb
Nnamdi Osuji <nnamdidanielosuji@gamil.com>
2022-03-24 13:58:32 +0100

    bug fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 // import { faBuilding, faCreditCard, faShop, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const FooterCont = styled.footer`
